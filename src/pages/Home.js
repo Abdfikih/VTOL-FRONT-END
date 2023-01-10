3498715359e06fd6d69bae97443e22af6d85ae84
Abdul <abdul.fikih@ui.ac.id>
2023-01-10 13:41:57 +0700

    test11

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -110,7 +110,7 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(
-        "https://drone-gemastik15.herokuapp.com/api/drone"
+        "https://vtol-cigritous-backend.herokuapp.com/api/drone"
       );
       setData(response.data);
       let lastElement = response.data.slice(-1)[0];
